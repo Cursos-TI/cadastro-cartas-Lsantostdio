cfe4f72cf8e733b5914e198db9f8b20d95a1db6e
Lsantostdio <github.stdio@gmail.com>
2025-09-18 23:36:32 +0000

    Definição de Variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
+
+
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+
+char Cidade_one [50] = "RiodeJaneiro";
+int População = 6.730729;
+float Área = 1.200330 km²;
+float Pib = 53.07823;
+float Qtd_Pontos_Turisticos = 
+char Código de Carta [10] = "R01";
+char Estado = 'R';
 
   // Área para entrada de dados
 
