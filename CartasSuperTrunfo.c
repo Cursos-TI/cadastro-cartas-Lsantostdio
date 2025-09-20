7b6ea968a076fc52083fa32391a25b2cfff20c9e
Lsantostdio <github.stdio@gmail.com>
2025-09-20 16:45:35 +0000

    Criação da Carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,8 +11,30 @@ int main(){
     float PIB = 53078.23;
     int pts_turisticos = 13; 
 
-    printf("Desafio Super Trunfo Nível Novato\n");
-    
+    // Mensangem de boas - vindas 
+    printf("Desafio Super Trunfo Nível Novato - CIDADE_RJOTA\n");
+
+    /* Definição das funções de entrada e saída: (printf- saída) / (scanf - entrada) */
+    printf("Digite a primeira a letra do nome da estado: ");
+    scanf("%c", &estado);
+    printf(" O primeira letra do nome da cidade é: %c\n", estado);
+
+    // Digitação do código da cidade
+    printf("Digite o código da cidade");
+    scanf("%s", cod_cidade);
+    printf("O código da cidade é: %s\n", cod_cidade);
+
+    // Digitaçãp do nome da cidade
+    printf("Digite o nome da cidade, conhecida como Cidade Maravilhosa: ");
+    scanf("%s", nome_cidade);
+    printf("O nome da cidade é %s\n", nome_cidade);
+
+    // Digitação da quantidade de habitantes da cidade
+    printf("Digite a quantidade de habitantes: ");
+    scanf("%d", &populacao);
+    printf("A quantidade da população da Cidade do Rio de Janeiro é: %d\n", populacao);
+
+
 
 
 
