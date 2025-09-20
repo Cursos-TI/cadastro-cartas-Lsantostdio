ea59f0cdc28c9fdeecde80e687dd82cd655ee0b1
Lsantostdio <github.stdio@gmail.com>
2025-09-20 14:42:54 +0000

    Definição de entradas e saídas da população

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
 
+int main(){
 
+    // Adição das variáveis da cidade 01
+    char estado = 'R';
+    char cod_cidade [10] = "R01";
+    char nome_cidade [30] = "RioDeJaneiro";
+    int populacao = 6211.223;
+    float area_km² = 1200.330;
+    float PIB = 53078.23;
+    int pts_turisticos = 13; 
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
-int main() {
-  // Definição das variáreis do Estado 01
-  char estado_01 = 'R';
-  char codigo_cidade_01 [10] = "R01";
-  char cidade_01 [50] = "RiodeJaneiro";
-  int ptsturist01 = 8;
-  int populacao_01 = 6730729;
-  float area_01 = 1.200330;
-  float pib_01 = 53.0782;
-
-    // Área para entrada de dados
-      printf(" Digite a sigla do primeiro estado: "); 
-        scanf("%c", &estado_01);
-          printf(" A sigla do primeiro estado é: %c\n", estado_01);
-
-          printf(" Digite o código da cidade 01: ");
-          scanf("%s", codigo_cidade_01);
-          printf("o código da sua cidade é: %s\n", codigo_cidade_01);
+    printf("Desafio Super Trunfo Nível Novato\n");
+    
 
 
 
 
-            // Área para exibição dos dados da cidade
 
-            return 0;
-            } 
-            
+
+}
