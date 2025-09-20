6cae30e4e680516fa7c2b51c67290c66b60678eb
Lsantostdio <github.stdio@gmail.com>
2025-09-20 14:01:42 +0000

    Nova codificação para maxificação do conteúdo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,20 +7,23 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
-
-  char Estado = 'R';
-  char Código_Carta [10] = "R01";
-  char Cidade [50] = "RiodeJaneiro";
-  int NúmeroPontosTurísticos = 8;
-  int População = 6730729;
-  float Área = 1.200330;
-  float PIB = 53.0782;
+  // Definição das variáreis do Estado 01
+  char estado_01 = 'R';
+  char codigo_cidade_01 [10] = "R01";
+  char cidade_01 [50] = "RiodeJaneiro";
+  int ptsturist01 = 8;
+  int populacao_01 = 6730729;
+  float area_01 = 1.200330;
+  float pib_01 = 53.0782;
 
     // Área para entrada de dados
-      printf(" Digite a sigla do primeiro estado: ");
-        scanf("%c", &Estado);
-          printf(" A sigla do primeiro estado é: %c\n", Estado);
+      printf(" Digite a sigla do primeiro estado: "); 
+        scanf("%c", &estado_01);
+          printf(" A sigla do primeiro estado é: %c\n", estado_01);
+
+          printf(" Digite o código da cidade 01: ");
+          scanf("%s", codigo_cidade_01);
+          printf("o código da sua cidade é: %s\n", codigo_cidade_01);
 
 
 
